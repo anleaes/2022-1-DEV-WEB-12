b7eb6fc13285d41941aadf58c97b8c286454bc96
Pedro Quadros <p.quadros.a@gmail.com>
2022-06-24 00:06:01 -0300

    Teste de up no git

---
+++ galeriaapp/galeriaapp/apps/tipo/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import TipoForm
+from .models import Tipo
+
+# Create your views here.
+
+def add_tipo(request):
+    template_name = 'tipo/add_tipo.html'
+    context = {}
+    if request.method == 'POST':
+        form = TipoForm(request.POST)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.save()
+            form.save_m2m()
+            return redirect('tipo:list_tipo')
+    form = TipoForm()
+    context['form'] = form
+    return render(request, template_name, context)
+
+def list_tipos(request):
+    template_name = 'tipo/list_tipo.html'
+    tipo = Tipo.objects.filter()
+    context = {
+        'tipo': tipo
+    }
+    return render(request, template_name, context)
+
+def edit_tipo(request, id_tipo):
+    template_name = 'tipo/add_tipo.html'
+    context ={}
+    tipo = get_object_or_404(Tipo, id=id_tipo)
+    if request.method == 'POST':
+        form = TipoForm(request.POST, instance=tipo)
+        if form.is_valid():
+            form.save()
+            return redirect('tipo:list_tipo')
+    form = TipoForm(instance=tipo)
+    context['form'] = form
+    return render(request, template_name, context)
+
+def delete_tipo(request, id_tipo):
+    tipo = Tipo.objects.get(id=id_tipo)
+    tipo.delete()
+    return redirect('tipo:list_tipo')
